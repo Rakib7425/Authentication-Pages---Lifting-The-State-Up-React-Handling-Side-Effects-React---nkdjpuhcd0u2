019f4d2fb2f289f9e794bc20a4832e1416626c7e
Rakibul Islam <103498045+Rakib7425@users.noreply.github.com>
2023-08-02 09:30:07 +0530

    Update Signup.js

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React,{useRef} from 'react';
+import { useNavigate } from 'react-router';
+import { signup } from '../api/auth';
+
 export const Signup = () =>{
+    const navigate = useNavigate()
+    const emailRef = useRef()
+    const pwRef = useRef()
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        const email = emailRef.current.value
+        const password = pwRef.current.value
+        console.log(email,password);
+        signup({email,password}).then(a=>navigate('/signin')).catch(c=>console.log(c))
+    }
+    
     return(
         <div id="signin-page">
             <h1>Sign Up page</h1>
-
+            <form onSubmit={handleSubmit}>
+                <input type="email" placeholder='email' ref={emailRef} required/>
+                <br />
+                <input type="password" placeholder='password' ref={pwRef} required/>
+                <br />
+                <button type='submit'>Sign up</button>
+            </form>
         </div>
     )
 }
