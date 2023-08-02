8cd4fc2b5c658fdcc75ea0763f6eb2b6d5e238c0
Rakibul Islam <103498045+Rakib7425@users.noreply.github.com>
2023-08-02 09:29:34 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Navigate, useNavigate } from 'react-router';
 
 export const Home = () =>{
-
+    const isLoggedIn = localStorage.getItem('isLoggedIn')
+    const navigate = useNavigate()
+    if(!isLoggedIn || isLoggedIn != 'true') return <Navigate to='/signin' />
+    const handleLogout = ()=>{
+        localStorage.removeItem('isLoggedIn')
+        navigate('/signin')
+    }
+    
     return(
         <div id="home-page">
             Home page
+            <button onClick={handleLogout} id="logout-btn">Logout</button>
         </div>
     )
 }
